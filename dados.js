756d9e1a750414e15d3e39ee069fee56e8737326
JheyDev <jennyfer.souzar@gmail.com>
2024-09-04 16:39:56 -0300

    Add Js Documents

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+      titulo: "Magneto",
+      descricao: "O Mestre do Magnetismo, mutante com a habilidade de manipular campos magnéticos, ele possui uma força inigualável e a capacidade de controlar qualquer objeto metálico.",
+      link: "https://pt.wikipedia.org/wiki/Magneto"
+    },
+    {
+      titulo: "Professor Xavier",
+      descricao: "Um poderoso telepata e fundador dos X-Men, Professor Xavier utiliza seus poderes mentais para proteger mutantes e promover a paz entre mutantes e humanos.",
+      link: "https://pt.wikipedia.org/wiki/Professor_Xavier"
+    },
+    {
+      titulo: "Fênix",
+      descricao: "Uma força cósmica poderosa que se manifesta em hospedeiros mutantes, a Fênix concede poderes quase ilimitados, como telecinese, telepatia e a capacidade de manipular a energia cósmica.",
+      link: "https://pt.wikipedia.org/wiki/F%C3%A9nix_For%C3%A7a"
+    }
+  ];
