a1b14450f09d3dbe127dca165f65a1040e32cb2d
JheyDev <jennyfer.souzar@gmail.com>
2024-09-10 00:18:38 -0300

    Add Personagens

--- dados.js
+++ dados.js
@@ -12,9 +12,79 @@ let dados = [
       tags: " mente controlar mentes professor "
     },
     {
-      titulo: "Fênix",
+      titulo: "Jean Grey",
       descricao: "Uma força cósmica poderosa que se manifesta em hospedeiros mutantes, a Fênix concede poderes quase ilimitados, como telecinese, telepatia e a capacidade de manipular a energia cósmica.",
+      imagem: "https://www.hqzona.com.br/wp-content/uploads/2018/06/bdsrz9dkqrtqsotvlbfg-1.jpg",
       link: "https://pt.wikipedia.org/wiki/F%C3%A9nix_For%C3%A7a",
       tags: " fenix controlar mentes ruiva "
+    },
+    {
+      titulo: "Wolverine",
+      descricao: "Um mutante com a habilidade de cura acelerada, garras retráteis e sentidos aguçados, Wolverine é um dos membros mais poderosos dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Wolverine",
+      imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTFuyNK10iP7P5i--j8cBsXLJbqv---6X6oQ&s",
+      tags: "wolverine garras cura acelerada"
+    },
+    {
+      titulo: "Ciclope",
+      descricao: "Um mutante com a habilidade de disparar rajadas ópticas, Ciclope é um dos primeiros recrutas do Professor Xavier e um dos líderes dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Ciclope_(X-Men)",
+      imagem: "https://i0.wp.com/ovicio.com.br/wp-content/uploads/2024/03/20240322-e82e3bad-033a-47e1-9ff8-f17dd25c44ed.jpg?resize=555%2C555&ssl=1",
+      tags: "ciclope rajadas ópticas líder"
+    },
+    
+    {
+      titulo: "Fera",
+      descricao: "Um mutante com a habilidade de se transformar em uma criatura felina, Fera é um dos membros mais agilizados e ágeis dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Fera_(X-Men)",
+      tags: "fera transformação felina agilidade"
+    },
+    {
+      titulo: "Tempestade",
+      descricao: "Uma mutante com a habilidade de controlar o clima, Tempestade é uma das mais poderosas mutantes do mundo e uma das líderes dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Tempestade_(X-Men)",
+      tags: "tempestade controle clima líder"
+    },
+    {
+      titulo: "Noturno",
+      descricao: "Um mutante com a habilidade de teleportação, Noturno é um dos membros mais misteriosos e poderosos dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Noturno_(X-Men)",
+      tags: "noturno teleportação misterioso"
+    },
+    {
+      titulo: "Gambit",
+      descricao: "Um mutante com a habilidade de manipular explosivos, Gambit é um dos membros mais charmosos e habilidosos dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Gambit_(X-Men)",
+      tags: "gambit explosivos charme habilidade"
+    },
+    {
+      titulo: "Rogue",
+      descricao: "Uma mutante com a habilidade de absorver a memória e a força vital de outros, Rogue é uma das mais poderosas mutantes do mundo e uma das líderes dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Rogue_(X-Men)",
+      tags: "rogue absorção memória força vital"
+    },
+    {
+      titulo: "Jubileu",
+      descricao: "Uma mutante com a habilidade de criar explosões de luz e som, Jubileu é uma das mais jovens e dinâmicas membros dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Jubileu_(X-Men)",
+      tags: "jubileu explosões luz som jovem dinâmico"
+    },
+    {
+      titulo: "Kitty Pryde",
+      descricao: "Uma mutante com a habilidade de se tornar intangível e caminhar através de objetos sólidos, Kitty Pryde é uma das mais habilidosas e inteligentes membros dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Kitty_Pryde",
+      tags: "kitty pryde intangível objetos sólidos habilidosa inteligente"
+    },
+    {
+      titulo: "Colossus",
+      descricao: "Um mutante com a habilidade de transformar seu corpo em aço orgânico, Colossus é um dos membros mais fortes e leais dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Colossus_(X-Men)",
+      tags: "colossus aço orgânico forte leal"
+    },
+    {
+      titulo: "Storm Shadow",
+      descricao: "Um mutante com a habilidade de se transformar em uma sombra viva, Storm Shadow é um dos membros mais misteriosos e habilidosos dos X-Men.",
+      link: "https://pt.wikipedia.org/wiki/Storm_Shadow",
+      tags: "storm shadow sombra viva misterioso habilidoso"
     }
   ];
